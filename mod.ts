13ab7c5c7d752643d9f137d7997566cc6eafb7ad
Kan <57884127+chakrakan@users.noreply.github.com>
2020-10-29 03:00:26 -0400

    Updated mod.ts
    Updating the `header` field to `skipFirstRow` to reflect changes as per the [latest documentation](https://doc.deno.land/https/deno.land/std/encoding/csv.ts#ParseOptions) for the csv encoding module.

--- mod.ts
+++ mod.ts
@@ -15,7 +15,7 @@ async function loadPlanetData() {
   const bufReader = new BufReader(file);
   
   const result = await parse(bufReader, {
-    header: true,
+    skipFirstRow: true,
     comment: "#",
   });
 
